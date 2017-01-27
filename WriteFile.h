6e2b701f0dda84e6775129973c3fc5747002b0bc
Andrew Jeromy Coburn <ajcoburn42@students.tntech.edu>
2017-01-26 18:30:52 -0600

    replaced 'struct' with 'class' and added pvt/pblc

--- WriteFile.h
+++ WriteFile.h
@@ -5,16 +5,24 @@
 
 #include <fstream>
 using namespace std;
-
-struct WriteFile
+/*
+	changed "struct" to "class" & added private & public fields
+	modified prototype definitions to include constructor and destructor
+	-Jeromy Coburn
+*/
+class WriteFile
 {
-   ofstream output_file;
-   bool closed;
+	private:
+	   ofstream output_file;
+	   bool closed;
+	   
+	public:
+		WriteFile* createWriteFile(const char* file_name);
+		~WriteFile*(); //destructor
+		void writeLine(WriteFile* wf, String* line);
+		void close(WriteFile* wf);
 };
 
-WriteFile* createWriteFile(const char* file_name);
-void destroyWriteFile(WriteFile* wf);
-void writeLine(WriteFile* wf, String* line);
-void close(WriteFile* wf);
+
 
 #endif
